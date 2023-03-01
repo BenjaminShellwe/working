702028014a16b671f06565b76b06716c164b3aa4
benjaminshellwe <benjaminshellwe@outlook.com>
2023-03-01 18:04:26 +0800

    2023年3月1日

---
+++ src/main/java/top/shellwe/working/PCIterator.java
@@ -0,0 +1,18 @@
+package top.shellwe.working;
+
+/**
+ * @Auther: Benjamin Thomas Shellwe
+ * @Date: 2023/3/1 - 03 - 01 - 14:25
+ * @Description: top.shellwe.working
+ * @version: 1.0
+ */
+
+public class PCIterator {
+    //获取二维对象
+    String[] matrixOne;
+    String[] matrixTwo;
+    String[] matrixFinal;
+    //组合二维对象
+
+    //输出二维对象
+}
